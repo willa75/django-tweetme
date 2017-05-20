69b580f5adec1874f8cfdcc0ecaff862ee5c67e8
willa75 <anthonywi99@yahoo.com>
2017-05-20 17:23:33 -0400

    Added tweets with time stamp

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=140)
+    timestamp = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.content)
