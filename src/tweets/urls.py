7e01482a1547d0d78b37c3eca49fe69deab200cd
willa75 <anthonywi99@gmail.com>
2017-08-25 16:14:47 -0400

    Finished adding url routing for tweets

---
+++ src/tweets/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from .views import TweetDetailView, TweetListView
+
+urlpatterns = [
+    #url(r'^admin/', admin.site.urls),
+    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
+]
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, ListView
+from .models import Tweet
 # Create your views here.
+
+class TweetDetailView(DetailView):
+	queryset = Tweet.objects.all()
+	template_name = "tweets/detail_view.html"
+
+	def get_object(self):
+		pk = self.kwargs.get("pk")
+		return Tweet.objects.get(id=pk)
+
+class TweetListView(ListView):
+	queryset = Tweet.objects.all()
+	template_name = "tweets/list_view.html"
+
+
+def tweet_detail_view(request, id=1):
+	obj = Tweet.objects.get(id=id)
+	print(obj)
+	context = {
+		"object": obj
+	}
+	return render(request, "tweets/detail_view.html", context)
+
+def tweet_list_view(request, id=1):
+	queryset = Tweet.objects.all()
+	print(queryset)
+	for obj in queryset:
+		print(obj.content)
+	context = {
+		"object_list": queryset
+	}
+	return render(request, "tweets/list_view.html", context)
