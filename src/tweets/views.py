2951386c8e065b31cec1fa6ad20178c2a2964b21
willa75 <anthonywi99@gmail.com>
2017-11-20 11:59:00 -0500

    Added login required for create view

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,19 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, CreateView
 
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin
 from .models import Tweet
 
 # Create your views here.
 
-class TweetCreateView(CreateView):
+class TweetCreateView(LoginRequiredMixin, FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = "tweets/create_view.html"
     success_url = "/tweet/create/"
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+    login_url = '/admin/'
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
