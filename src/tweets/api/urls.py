0b22d9c2c1bc3160b103386f16446ef1d29866ac
willa75 <anthonywi99@gmail.com>
2018-02-16 10:14:16 -0500

    Restricted number of chars to 140 on the frontend

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
+	TweetCreateAPIView,
     TweetListAPIView
     )
 
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
-    # url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
+    url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/create/
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
     # url(r'^(?P<pk>\d+)/edit$', TweetUpdateView.as_view(), name='edit'), # /tweet/1/
     # url(r'^(?P<pk>\d+)/delete$', TweeDeleteView.as_view(), name='delete'), # /tweet/1/
--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -1,15 +1,25 @@
 from django.db.models import Q
 from rest_framework import generics
+from rest_framework import permissions
 
 from .serializers import TweetModelSerializer
 from tweets.models import Tweet
+
+
+class TweetCreateAPIView(generics.CreateAPIView):
+    serializer_class = TweetModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer 
 
     def get_queryset(self, *args, **kwargs):
-        qs = Tweet.objects.all()
+        qs = Tweet.objects.all().order_by("-timestamp")
         query = self.request.GET.get("q", None)
         if query is not None:
            qs = qs.filter(
